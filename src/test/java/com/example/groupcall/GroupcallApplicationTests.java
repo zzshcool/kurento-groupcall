2bcde8772860379418a4a64205c153a2ff4514d8
DESKTOP-RA21PC1\jasoncc.chen <jason.chen@dkdtrillion.com>
2021-04-12 13:49:06 +0800

    init

---
+++ src/test/java/com/example/groupcall/GroupcallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.groupcall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupcallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
